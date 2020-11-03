598039d59c64a2b93e2d7cf7e9b212de3f50c701
kralynch <kralynch@homeimprovementbb.com>
2020-11-02 21:53:01 -0400

    Modified to send email on offline only to users whose email address is specified in openfire user database

--- src/java/com/tempstop/emailOnAway.java
+++ src/java/com/tempstop/emailOnAway.java
@@ -1,6 +1,5 @@
 package com.tempstop;
 
-import org.jivesoftware.openfire.MessageRouter;
 import org.jivesoftware.openfire.session.Session;
 import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
@@ -12,31 +11,30 @@ import org.jivesoftware.openfire.user.UserManager;
 import org.jivesoftware.openfire.user.UserNotFoundException;
 import org.jivesoftware.openfire.user.User;
 import org.jivesoftware.openfire.PresenceManager;
-import org.jivesoftware.openfire.vcard.VCardManager;
-import org.jivesoftware.util.JiveGlobals;
 import org.jivesoftware.util.EmailService;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //import java.io.File;
 import java.io.*;
 
 public class emailOnAway implements Plugin, PacketInterceptor {
 
+    private static final Logger Log = LoggerFactory.getLogger(emailOnAway.class);
+    
     private InterceptorManager interceptorManager;
     private UserManager userManager;
     private PresenceManager presenceManager;
     private EmailService emailService;
-    private MessageRouter messageRouter;
-    private VCardManager vcardManager;
     
     public emailOnAway() {
         interceptorManager = InterceptorManager.getInstance();
-    emailService = EmailService.getInstance();
-        messageRouter = XMPPServer.getInstance().getMessageRouter();
-    presenceManager = XMPPServer.getInstance().getPresenceManager();
-    userManager = XMPPServer.getInstance().getUserManager();
-    vcardManager = VCardManager.getInstance();
+	    emailService = EmailService.getInstance();
+	    presenceManager = XMPPServer.getInstance().getPresenceManager();
+	    userManager = XMPPServer.getInstance().getUserManager();
     }
 
     public void initializePlugin(PluginManager pManager, File pluginDirectory) {
@@ -49,76 +47,41 @@ public class emailOnAway implements Plugin, PacketInterceptor {
         interceptorManager.removeInterceptor(this);
     }
 
-    private Message createServerMessage(String to, String from, String emailTo) {
-        Message message = new Message();
-        message.setTo(to);
-        message.setFrom(from);
-        message.setSubject("I'm away");
-        if (JiveGlobals.getBooleanProperty("plugin.emailonaway.showemail", true)) { 
-            message.setBody("I'm currently away. Your message has been forwarded to my email address  (" + emailTo + ")."); 
-            } else { message.setBody("I'm currently away. Your message has been forwarded to my email address."); }
-        return message;
-    }
-
     public void interceptPacket(Packet packet, Session session, boolean read,
             boolean processed) throws PacketRejectedException {
     
-    String emailTo = null;
-    String emailFrom = null;
-
     if((!processed) && 
-        (!read) && 
         (packet instanceof Message) && 
-        (packet.getTo() != null)) { 
-
+        (packet.getTo() != null)) 
+    { 
         Message msg = (Message) packet;
         
         if(msg.getType() == Message.Type.chat) {
-        try {
+        try 
+        {
             User userTo = userManager.getUser(packet.getTo().getNode());
-            if(presenceManager.getPresence(userTo).toString().toLowerCase().indexOf("away") != -1) {
-            // Status isn't away
-            if(msg.getBody() != null) {
-                // Build email/sms
-                // The to email address
-                emailTo = vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL");
-                if(emailTo == null || emailTo.length() == 0) {
-                emailTo = vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL:USERID");
-                if(emailTo == null || emailTo.length() == 0) {
-                    emailTo = userTo.getEmail();
-                    if(emailTo == null || emailTo.length() == 0) {
-                    emailTo = packet.getTo().getNode() + "@" + packet.getTo().getDomain();
-                    }
-                }
-                }
-                // The From email address
-                User userFrom = userManager.getUser(packet.getFrom().getNode());
-                emailFrom = vcardManager.getVCardProperty(userFrom.getUsername(), "EMAIL");
-                if(emailFrom == null || emailFrom.length() == 0) {
-                emailFrom = vcardManager.getVCardProperty(userFrom.getUsername(), "EMAIL:USERID");
-                if(emailFrom == null || emailFrom.length() == 0) {
-                    emailFrom = userFrom.getEmail();
-                    if(emailFrom == null || emailFrom.length() == 0) {
-                    emailFrom = packet.getFrom().getNode() + "@" + packet.getFrom().getDomain();
-                    }
-                }
-                }
-
-//			    System.err.println(vcardManager.getVCardProperty(userTo.getUsername(), "EMAIL:USERID"));
-                // Send email/sms
-                // if this is an sms... modify the recipient address
-                emailService.sendMessage(userTo.getName(), 
-                emailTo, 
-                userFrom.getName(), 
-                emailFrom,
-                "IM",
-                msg.getBody(), 
-                null);
-                
-                // Notify the sender that this went to email/sms
-                messageRouter.route(createServerMessage(packet.getFrom().getNode() + "@" + packet.getFrom().getDomain(), packet.getTo().getNode() + "@" + packet.getTo().getDomain(), emailTo));
-
-            }
+            //if online strinf is not found in users presence
+            if(presenceManager.getPresence(userTo) == null) 
+            {
+	            //if body is not empty
+	            if((msg.getBody() != null)&&(msg.getBody().trim().length() > 0)) 
+	            {
+	                // The From email address
+	                User userFrom = userManager.getUser(packet.getFrom().getNode());
+	              
+	                //only send if the user has an email specified
+	                if( userTo.getEmail() != null && userTo.getEmail().length() > 0 )
+	                {
+	                	emailService.sendMessage(
+	                			userTo.getName(), 				//To Name in email header
+		                		userTo.getEmail(), 				//Destination email address
+		                		userFrom.getName(), 			//From Display Name in email header
+		                		emailService.getUsername(),		//From address in email header
+		                		"Offline Message",				//Email Subject
+		                		msg.getBody(), 					//Email Body
+		                		null);							//Email Body Html
+	                }
+	            }
             }
         } catch (UserNotFoundException e) {
         }
